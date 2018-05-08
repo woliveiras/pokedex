9520ca90f1ee6fe40d8911e7e95f6204d0262342
woliveiras <w.oliveira542@gmail.com>
2018-05-08 10:50:45 -0300

    Start project with create-react-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default App;
