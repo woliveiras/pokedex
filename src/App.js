ab34fc050ed441a54b10ae048bcb2c194e6af203
woliveiras <w.oliveira542@gmail.com>
2018-05-08 13:30:32 -0300

    Add state and state manager to visor component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isVisorActive: false
+    }
+  }
+
+  toggleVisor = () => {
+    this.setState({
+      isVisorActive: !this.state.isVisorActive
+    })
+  }
+
   render() {
     return (
       <div className="pokedex">
         <div className="top-bar"></div>
-        <div className="visor">
-          <button class="visor__action-button"></button>
+        <div 
+          className="visor"
+          style={{ height: this.state.isVisorActive ? '420px' : '' }}>
+          <button 
+            className="visor__action-button" 
+            onClick={ this.toggleVisor }>
+          </button>
         </div>
         <div className="bottom-bar"></div>
       </div>
